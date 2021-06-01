7254a399122c1f8beb23139cda374bf7a408aff3
Aditya8Prakash <60220908+Aditya8Prakash@users.noreply.github.com>
2021-06-01 16:03:29 +0530

    Update automatic_file_generator.py

--- automatic_file_generator.py
+++ automatic_file_generator.py
@@ -1,7 +1,7 @@
 file_name = input("File name : ")
 file_ext = input("File extention : ")
 file_data = input("File data (optional) : ")
-file_dest = input("File destination (optional / leave blanck to create at './') : ")
+file_dest = input("File destination (optional or '.' blanck to create at './') : ")
 file = file_name+'.'+file_ext
 file_path = file_dest+'/'+file
 with open(file_path, 'w',encoding='utf-8') as fp:
